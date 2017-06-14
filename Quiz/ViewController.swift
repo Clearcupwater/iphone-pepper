68263eb680b42f9fa7be0eca8d6e9dd202a74cda
Tadmin <tadmin@Tadmins-iMac.local>
2017-06-13 23:14:15 -0700

    initial push

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -10,16 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = [
+    "What is 7+7",
+    "What is capital of Vermont",
+    "What is cognac made from?"
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    ]
+    
+    let answers: [String] = [
+    "14",
+    "Montepelier",
+    "Grapes"
+    
+    ]
+    var currentQuestionIndex: Int = 0;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    @IBAction func showNextQuestion (_ send: UIButton){
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+                currentQuestionIndex = 0
+            
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+        
+        
+    
     }
-
+    @IBAction func showAnswer(_ send: UIButton){
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+        
+        
+    }
 
 }
 
