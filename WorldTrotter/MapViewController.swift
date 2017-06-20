b9a892ce8162bceb20b2e8b12f9d414d556e4062
Tadmin <tadmin@Tadmins-iMac.local>
2017-06-19 20:25:27 -0700

    Playing with Programtic Views part 2

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -7,8 +7,40 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        mapView = MKMapView()
+        
+        view = mapView
+    
+        let segmentedControl = UISegmentedControl(items: ["Satellite","Hybrid","Satellite"])
+        segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: view.topAnchor)
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+        let trainingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+    
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trainingConstraint.isActive = true
+        
+    }
+    
+
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print ("MapView Controller loaded its view")
