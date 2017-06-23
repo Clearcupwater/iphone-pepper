873bffa4ce9fa6d0ce561b2dfd1c3a3aa871b334
Virtual <virtual@virtuals-macbook-pro.local>
2017-06-23 14:16:04 -0700

    add stack 3

---
+++ webServices/PhotosViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PhotosViewController.swift
+//  webServices
+//
+//  Created by Virtual on 6/23/17.
+//  Copyright © 2017 Tadmin. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    
+}
