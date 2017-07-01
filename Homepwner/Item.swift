9b650f241a64cd1c7a46e2742c4179d7e657665a
Virtual <virtual@virtuals-macbook-pro.local>
2017-06-30 17:39:06 -0700

    Added Decoders

--- Homepwner/Item.swift
+++ Homepwner/Item.swift
@@ -4,7 +4,7 @@
 
 import Foundation
 
-class Item: NSObject {
+class Item: NSObject, NSCoding {
     
     var name: String
     var valueInDollars: Int
@@ -43,5 +43,26 @@ class Item: NSObject {
             self.init(name: "", serialNumber: nil, valueInDollars: 0)
         }
     }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: "name")
+        aCoder.encode(dateCreated, forKey: "datecreated")
+        aCoder.encode(itemKey, forKey: "itemKey")
+        aCoder.encode(serialNumber, forKey: "serialNumber")
+        aCoder.encode(valueInDollars, forKey:"valueInDollars")
+        
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        name = aDecoder.decodeObject(forKey: "name") as! String
+        dateCreated = aDecoder.decodeObject(forKey: "dateCreated") as! Date
+        itemKey = aDecoder.decodeObject(forKey: "itemKey") as! String
+        serialNumber = aDecoder.decodeObject(forKey: "serialNumber") as? String
+        valueInDollars = aDecoder.decodeInteger(forKey: "valueInDollars")
+        
+        
+    }
+    
+    
     
 }
