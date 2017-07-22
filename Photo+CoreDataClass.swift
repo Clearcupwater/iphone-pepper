1be9ee7107910eeac6b2e71d99bc0fe64ae3109a
Virtual <virtual@Virtuals-MacBook-Pro.local>
2017-07-21 17:20:08 -0700

    Intialize

---
+++ Photo+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Photo+CoreDataClass.swift
+//  Photorama
+//
+//  Created by Virtual on 7/21/17.
+//  Copyright © 2017 Big Nerd Ranch. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Photo)
+public class Photo: NSManagedObject {
+
+}
